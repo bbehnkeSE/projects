ae96a1d0cf56dd429acca48ee6f2a913f6a8fb68
bbehnkeSE <bbehnke@kent.edu>
2022-02-02 01:19:11 -0500

    can now set sleep timer

--- school/systems_programming/watch/utmplib.c
+++ school/systems_programming/watch/utmplib.c
@@ -1,79 +1,68 @@
-
-#include	<stdio.h>
-#include	<fcntl.h>
-#include	<sys/types.h>
-#include	<utmp.h>
-
-/*
- *	library of functions to do buffered reads from utmp file
- *	
- *	reads NRECS per read and then doles them out from the
- *	buffer
+/* utmplib.c  - functions to buffer reads from utmp file 
+ *
+ *      functions are
+ *              utmp_open( filename )   - open file
+ *                      returns -1 on error
+ *              utmp_next( )            - return pointer to next struct
+ *                      returns NULL on eof
+ *              utmp_close()            - close file
+ *
+ *      reads NRECS per read and then doles them out from the buffer
  */
+#include        <stdio.h>
+#include        <fcntl.h>
+#include        <sys/types.h>
+#include        <utmp.h>
 
-#define	NRECS	16
-#define	NULLUT	((struct utmp *)NULL)
-#define	UTSIZE	(sizeof(struct utmp))
+#define NRECS   16
+#define NULLUT  ((struct utmp *)NULL)
+#define UTSIZE  (sizeof(struct utmp))
 
-static	char	utmpbuf[NRECS * UTSIZE];		/* storage	*/
-static	int	num_recs;				/* num stored	*/
-static	int	cur_rec;				/* next to go	*/
-static	int	fd_utmp = -1;				/* read from	*/
+static  char    utmpbuf[NRECS * UTSIZE];                /* storage      */
+static  int     num_recs;                               /* num stored   */
+static  int     cur_rec;                                /* next to go   */
+static  int     fd_utmp = -1;                           /* read from    */
 
-/*
- *	buffered reads from /etc/utmp
- *	functions are
- *		utmp_open( filename )	- open file
- *			returns -1 on error
- *		utmp_next( )		- return pointer to next struct
- *			returns NULL on eof
- *		utmp_close()		- close file
- */
-
-utmp_open( filename )
-char *filename;
+utmp_open( char *filename )
 {
-	if ( filename == NULL )
-		filename = UTMP_FILE;
-	fd_utmp = open( filename, O_RDONLY );		/* open it	*/
-	cur_rec = num_recs = 0;				/* no recs yet	*/
-	return fd_utmp;					/* report	*/
+        fd_utmp = open( filename, O_RDONLY );           /* open it      */
+        cur_rec = num_recs = 0;                         /* no recs yet  */
+        return fd_utmp;                                 /* report       */
 }
 
-struct utmp *
-utmp_next()
+struct utmp *utmp_next()
 {
-	struct utmp *recp;
+        struct utmp *recp;
 
-	if ( fd_utmp == -1 )				/* error ? 	*/
-		return NULLUT;
-	if ( cur_rec==num_recs && utmp_reload()==0 )	/* any more ?	*/
-		return NULLUT;
-					/* get address of next record	 */
-	recp = ( struct utmp *) &utmpbuf[cur_rec * UTSIZE];
-	cur_rec++;
-	return recp;
+        if ( fd_utmp == -1 )                            /* error ?      */
+                return NULLUT;
+        if ( cur_rec==num_recs && utmp_reload()==0 )    /* any more ?   */
+                return NULLUT;
+                                        /* get address of next record    */
+        recp = ( struct utmp *) &utmpbuf[cur_rec * UTSIZE];
+        cur_rec++;
+        return recp;
 }
 
-utmp_reload()
+int utmp_reload()
 /*
- *	read next bunch of records into buffer
+ *      read next bunch of records into buffer
  */
 {
-	int	amt_read;
+        int     amt_read;
 
-						/* read them in		*/
-	amt_read = read( fd_utmp , utmpbuf, NRECS * UTSIZE );	
+                                                /* read them in         */
+        amt_read = read( fd_utmp , utmpbuf, NRECS * UTSIZE );   
 
-						/* how many did we get?	*/
-	num_recs = amt_read/UTSIZE;
-						/* reset pointer	*/
-	cur_rec  = 0;
-	return num_recs;
+                                                /* how many did we get? */
+        num_recs = amt_read/UTSIZE;
+                                                /* reset pointer        */
+        cur_rec  = 0;
+        return num_recs;
 }
 
 utmp_close()
 {
-	if ( fd_utmp != -1 )			/* don't close if not	*/
-		close( fd_utmp );		/* open			*/
+        if ( fd_utmp != -1 )                    /* don't close if not   */
+                close( fd_utmp );               /* open                 */
 }
--- school/systems_programming/watch/watch.c
+++ school/systems_programming/watch/watch.c
@@ -6,13 +6,64 @@
  * Checks for the login/logout of one or more users
  */
 
+#include    <stdio.h>
+#include    <sys/types.h>
+#include    <utmp.h>
+#include    <fcntl.h>
+#include    <stdlib.h>
+#include    <unistd.h>      /* sleep() */
 
-#include <stdio.h>
-#include <utmp.h>
+void show_info(struct utmp *);
+int  checkNum (char cla[]);
 
 int main(int argc, char *argv[])
 {
-	
+    struct utmp *utbufp,        /* holds pointer to next rec    */
+                *utmp_next();   /* returns pointer to next  */
 
-	return 0;
+    int sleepTime;
+    if(checkNum(argv[1])) 
+        sscanf(argv[1], "%d", &sleepTime);  /* converts argv[1] to int */
+    else
+        sleepTime = 10;
+
+    /* infinite loop to keep checking users */
+    for(;;)
+    {
+        if (utmp_open(UTMP_FILE) == -1) {
+            perror(UTMP_FILE);
+            exit(1);
+        }
+
+        while ((utbufp = utmp_next()) != ((struct utmp *) NULL))
+            show_info(utbufp);
+        utmp_close();
+
+        sleep(sleepTime);
+    }
+    return 0;
+}
+
+/*
+ *  show info()
+ *  displays the contents of the utmp struct
+ *  in human readable form
+ *  displays nothing if record has no user name
+ */
+void show_info(struct utmp *utbufp)
+{
+    if (utbufp->ut_type != USER_PROCESS)
+        return;
+
+    printf("%-8.8s\n", utbufp->ut_name);      /* the logname  */
+}
+
+/* Checks if char array is an int */
+int checkNum(char cla[])
+{
+    for(int i = 0; cla[i] != 0; ++i)
+        if(!isdigit(cla[i]))
+            return 0;
+
+    return 1;
 }
