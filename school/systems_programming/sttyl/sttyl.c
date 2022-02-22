fc65565345eef4ee8422cd34a340c27e6e67f796
bbehnkeSE <bbehnke@kent.edu>
2022-02-21 20:53:20 -0500

    checks for '-'

--- school/systems_programming/sttyl/sttyl.c
+++ school/systems_programming/sttyl/sttyl.c
@@ -51,49 +51,85 @@ struct fl_info oflags[] =
 	{ 0      ,   NULL    }
 };
 
-void  noArgs(struct termios);
-void  printFlags(int, struct fl_info []);
+void  noArgs     (struct termios*);
+void  enable     (char*, struct termios*);
+void  disable    (char*, struct termios*);
+
+void  printFlags (int,   struct fl_info []);
+int   checkFlags (char*, struct fl_info []);
 
 
 int main(int argc, char *argv[])
 {
 	struct termios tio;
 	if(argc > 1)
-	{
-		if(tcgetattr(0, &tio) == -1)
+		for(int i = 1; i < argc; ++i)
 		{
-			perror("Unable to get info. Exiting...");
-			exit(1);
+			if(argv[i][0] == '-')
+				disable(argv[i], &tio);
+			else
+				continue;
 		}
-	}
 	else
-		noArgs(tio);
+		noArgs(&tio);
 
 	return 0;
 }
 
 
-void noArgs(struct termios tio)
+void disable(char* str, struct termios* tio)
 {
-	if(tcgetattr(0, &tio) == -1)
+	++str; 		/* removes '-' */
+	int i = checkFlags(str, iflags);
+	int l = checkFlags(str, lflags);
+	int o = checkFlags(str, oflags);
+
+	if(i)
+		printf("disable %s\n", iflags[i].fl_name);
+	else if(l)
+		printf("disable %s\n", lflags[l].fl_name);
+	else if(o)
+		printf("disable %s\n", oflags[o].fl_name);
+	else
+		printf("Unknown mode.\n");
+}
+
+
+void noArgs(struct termios *tio)
+{
+	if(tcgetattr(0, tio) == -1)
 	{
 		perror("Unable to get info. Exiting...");
 		exit(1);
 	}
 	/* Prints various keyboard commands */
 	printf("intr = ^%c; erase = ^%c; kill = ^%c; start = ^%c; stop = ^%c;\n",
-			tio.c_cc[VINTR]  - 1 + 'A',
-			tio.c_cc[VERASE] - 1 + 'A',
-			tio.c_cc[VKILL]  - 1 + 'A',
-			tio.c_cc[VSTART] - 1 + 'A',
-			tio.c_cc[VSTOP]  - 1 + 'A'
+			tio->c_cc[VINTR]  - 1 + 'A',
+			tio->c_cc[VERASE] - 1 + 'A',
+			tio->c_cc[VKILL]  - 1 + 'A',
+			tio->c_cc[VSTART] - 1 + 'A',
+			tio->c_cc[VSTOP]  - 1 + 'A'
 		  );
 
-	printFlags(tio.c_iflag, iflags);
-	printFlags(tio.c_lflag, lflags);
-	printFlags(tio.c_oflag, oflags);
+	printFlags(tio->c_iflag, iflags);
+	printFlags(tio->c_lflag, lflags);
+	printFlags(tio->c_oflag, oflags);
 		
 }
+
+
+/* Utility functions */
+
+int checkFlags(char* str, struct fl_info name[])
+{
+	for(int i = 0; name[i].fl_value; ++i)
+	{
+		if(!strcmp(str, name[i].fl_name))
+			return i;
+	}
+	return 0;
+}
+
 
 void printFlags(int flag, struct fl_info name[])
 {
