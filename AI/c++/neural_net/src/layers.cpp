0d839636a7411097965edf1ba35674a42e4414c8
bbehnkeSE <bbehnke@kent.edu>
2022-06-26 01:52:47 -0400

    predicts XOR results

--- AI/c++/neural_net/src/layers.cpp
+++ AI/c++/neural_net/src/layers.cpp
@@ -1,12 +1,64 @@
 #include "layers.hpp"
 
-Dense::Dense(int inputSize, int outputSize)
+
+//===========================================
+// Error functions
+//
+
+// Mean squared error
+double mse(Vector correct, Vector pred)
 {
-    weights = Eigen::MatrixXd::Random(inputSize, outputSize);
-    bias    = Eigen::MatrixXd::Random(outputSize, 1);
+    return (correct - pred).array().square().mean();
 }
 
-Eigen::MatrixXd Dense::forward(Eigen::MatrixXd input)
+Vector msePrime(Vector correct, Vector pred)
 {
-    return (weights * input) + bias;
+    return 2 * (pred - correct) / correct.rows();
+}
+
+
+//===========================================
+// Activation functions
+//
+
+//Hyperbolic tangent
+void hyperTan(Vector& vec)
+{
+    for(int i = 0; i < vec.rows(); ++i)
+        vec(i) = tanh(vec(i));
+}
+
+void hyperTanPrime(Vector& vec)
+{
+    for(int i = 0; i < vec.rows(); ++i)
+        vec(i) = 1 - pow(tanh(vec(i)), 2);
+}
+
+
+//===========================================
+// Layer functions
+//
+
+// Dense layer
+Dense::Dense(int inputSize, int outputSize)
+{
+    weights = Matrix::Random(outputSize, inputSize);
+    biases  = Vector::Random(outputSize);
+}
+
+Vector Dense::forward(Vector in)
+{
+    input = in;
+    return (weights * input) + biases;
+}
+
+Vector Dense::backward(Vector outputGrad, double learningRate)
+{
+    Matrix weightGrad = outputGrad * input.transpose();         // Calculate new weights              (dE/dW = dE/dY * X^t)
+    Vector inputGrad  = weights.transpose() * outputGrad;       // Calculate input for previous layer (dE/dX = W^t * dE/dY) 
+
+    weights          -= learningRate * weightGrad;              // Update weights
+    biases           -= learningRate * outputGrad;              // Update biases                      (dE/dB = dE/dY)
+
+    return inputGrad;
 }
--- AI/c++/neural_net/src/main.cpp
+++ AI/c++/neural_net/src/main.cpp
@@ -1,8 +1,79 @@
 #include "layers.hpp"
 
+void printVec(Vector vec)
+{
+    std::cout << "\nVector:\n" << vec << std::endl;
+}
+
 int main()
 {
-    Dense m(3, 3);
+    // Create XOR training data
+    Vector x1(2), x2(2), x3(2), x4(2);
+    x1 << 0, 0;
+    x2 << 0, 1;
+    x3 << 1, 0;
+    x4 << 1, 1;
+
+    Vector y1(1), y2(1), y3(1), y4(1);
+    y1 << 0;
+    y2 << 1;
+    y3 << 1;
+    y4 << 0;
+
+    std::vector<Vector> X { x1, x2, x3, x4 };
+    std::vector<Vector> Y { y1, y2, y3, y4 };
+
+    // Setup function pointers
+    void (*HTPtr) (Vector&) { &hyperTan };
+    void (*HTPPtr)(Vector&) { &hyperTanPrime };
+
+    // Create network
+    std::vector<Layer*> network
+    {
+        new Dense(2, 3),
+        new Activation<void(*)(Vector&)>(HTPtr, HTPPtr),
+        new Dense(3, 1),
+        new Activation<void(*)(Vector&)>(HTPtr, HTPPtr)
+    };
+
+    // Training loop
+    int    epochs       = 10000;
+    double learningRate = 0.1;
+    Vector output;
+    Vector gradient;
+
+    for(int i = 0; i < epochs; ++i)
+    {
+        for(int j = 0; j < X.size(); ++j)
+        {
+            // Forward propagation
+            output = X[j];
+            for(auto layer: network)
+                output = layer->forward(output);
+
+            // Backward propagation
+            gradient = msePrime(Y[j], output);
+            for(auto it = network.rbegin(); it != network.rend(); ++it)
+                gradient = (*it)->backward(gradient, learningRate);
+        }
+    }
+
+    std::cout << "\nFinal output:\n"
+              << "--------------------\n"
+              << "Correct : Prediction\n"
+              << "--------:-----------\n";
+    for(int i = 0; i < X.size(); ++i)
+    {
+        output = X[i];
+        for(auto layer: network)
+            output = layer->forward(output);
+
+        std::cout << "      " << Y[i] << " : " << output << std::endl;
+    }
+
+    // Free memory
+    for(auto it = network.begin(); it != network.end(); ++it)
+        delete &it;
 
     return 0;
 }
