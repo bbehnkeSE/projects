688f2b108b7912950f02c12c97efb0abe24acb53
bbehnkeSE <bbehnke@kent.edu>
2022-11-17 20:19:47 -0500

    Update server.py

--- school/data_security_and_privacy/project/client_files/client.py
+++ school/data_security_and_privacy/project/client_files/client.py
@@ -1,18 +1,27 @@
 from socket import *
 
-server_ip = "192.168.178.129"
-port = 9001
+def connectToServer():
+	server_ip = "192.168.178.129"
+	port = 9001
 
-clientSocket = socket(AF_INET, SOCK_STREAM)
-serverAddress = (server_ip, port)
+	clientSocket = socket(AF_INET, SOCK_STREAM)
+	serverAddress = (server_ip, port)
 
-message = "Test message"
+	message = "Connection test"
 
-clientSocket.connect(serverAddress)
-clientSocket.sendall(message.encode())
+	clientSocket.connect(serverAddress)
+	clientSocket.sendall(message.encode())
 
-response = clientSocket.recv(2048).decode()
+	response = clientSocket.recv(1024).decode()
 
-print(response)
+	if response == "Message received.":
+		print(response)
+		return clientSocket
+	else:
+		print('Unable to connect...')
+		clientSocket.close()
 
-clientSocket.close()
+
+def closeConnection(socket):
+	socket.sendall('Exit.')
+	socket.close()
