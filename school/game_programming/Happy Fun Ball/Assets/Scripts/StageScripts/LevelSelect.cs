e189e7dddd7e62aba438fec3664631f8256fcac6
bbehnkeSE <bbehnke@kent.edu>
2022-12-14 23:10:14 -0500

    HFB update

---
+++ school/game_programming/Happy Fun Ball/Assets/Scripts/StageScripts/LevelSelect.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class LevelSelect : MonoBehaviour
+{
+    public GameObject mainMenu;
+
+    public Button levelOneButton;
+    public Button levelTwoButton;
+    public Button levelThreeButton;
+    public Button levelFourButton;
+    public Button mainMenuButton;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        gameObject.SetActive(false);
+
+        levelOneButton.onClick.AddListener(loadLevelOne);
+        levelTwoButton.onClick.AddListener(loadLevelTwo);
+        levelThreeButton.onClick.AddListener(loadLevelThree);
+        levelFourButton.onClick.AddListener(loadLevelFour);
+
+        mainMenuButton.onClick.AddListener(returnToMainMenu);
+    }
+
+    private void returnToMainMenu()
+    {
+        mainMenu.SetActive(true);
+        gameObject.SetActive(false);
+    }
+
+    private void loadLevelOne()
+    {
+        SceneManager.LoadScene("Level_1");
+    }
+
+    private void loadLevelTwo()
+    {
+        SceneManager.LoadScene("Level_2");
+    }
+
+    private void loadLevelThree()
+    {
+        SceneManager.LoadScene("Level_3");
+    }
+
+    private void loadLevelFour()
+    {
+        SceneManager.LoadScene("Level_4");
+    }
+
+}
--- school/game_programming/Happy Fun Ball/Assets/Scripts/StageScripts/MainMenu.cs
+++ school/game_programming/Happy Fun Ball/Assets/Scripts/StageScripts/MainMenu.cs
@@ -6,13 +6,17 @@ using UnityEngine.UI;
 
 public class MainMenu : MonoBehaviour
 {
+    public  GameObject levelSelectMenu;
     public  Button playButton;
     public  Button quitButton;
+    public  Button levelSelectButton;
     private AudioClip successClip;
 
     private void Start()
     {
+        gameObject.SetActive(true);
         playButton.onClick.AddListener(newGame);
+        levelSelectButton.onClick.AddListener(selectLevelMenu);
         quitButton.onClick.AddListener(quitGame);
         successClip = Resources.Load<AudioClip>("Audio/ap_mbu/success");
     }
@@ -21,6 +25,12 @@ public class MainMenu : MonoBehaviour
     {
         AudioManager.Instance.SuccessSound(successClip);
         SceneManager.LoadScene("Level_1");
+    }
+
+    private void selectLevelMenu()
+    {
+        levelSelectMenu.SetActive(true);
+        gameObject.SetActive(false);
     }
 
     private void quitGame()
